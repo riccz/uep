84ffa1060ad3d6f42f0dbb943912004d2a4916e4
Riccardo Zanol <ricc@zanol.eu>
2017-11-02 09:59:00 +0100

    Plot with fixed chan_pGB

--- src/plot_fast_markov.py
+++ src/plot_fast_markov.py
@@ -24,22 +24,16 @@ def run_uep_map(params):
     print(".", end="")
     return res
 
-fixed_params = [{'avg_per': 1e-2,
+fixed_params = [{'chan_pGB': 1e-6,
+                 'overhead': 0.2,
+                 'k': 1000},
+                {'chan_pGB': 1e-6,
                  'overhead': 0.2,
                  'k': 500},
-                {'avg_per': 1e-2,
+                {'chan_pGB': 1e-6,
                  'overhead': 0.2,
-                 'k': 1000},
-                {'avg_per': 1e-2,
-                 'overhead': 0.2,
-                 'k': 5000},
-                {'avg_per': 1e-1,
-                 'overhead': 0.25,
-                 'k': 1000},
-                {'avg_per': 3e-1,
-                 'overhead': 0.25,
-                'k': 1000}]
-avg_bad_runs = np.linspace(1, 1500, 32).tolist()
+                 'k': 5000}]
+avg_bad_runs = np.linspace(1, 1500, 10).tolist()
 k0_fraction = 0.1
 
 base_params = simulation_params()
@@ -48,7 +42,7 @@ base_params.EF = 4
 base_params.c = 0.1
 base_params.delta = 0.5
 base_params.L = 4
-base_params.nblocks = 50
+base_params.nblocks = 100
 
 param_matrix = list()
 for p in fixed_params:
@@ -57,7 +51,7 @@ for p in fixed_params:
         params = simulation_params(base_params)
         k0 = int(k0_fraction * p['k'])
         params.Ks[:] = [k0, p['k'] - k0]
-        params.chan_pGB = 1/br * p['avg_per'] / (1 - p['avg_per'])
+        params.chan_pGB = p['chan_pGB']
         params.chan_pBG = 1/br
         params.overhead = p['overhead']
         param_matrix[-1].append(params)
@@ -70,6 +64,21 @@ with multiprocessing.Pool() as pool:
 
 plt.figure()
 plt.gca().set_yscale('log')
+chan_pGBs = [ p['chan_pGB'] for p in fixed_params ]
+chan_pGBs = sorted(set(chan_pGBs))
+brs = np.linspace(avg_bad_runs[0], avg_bad_runs[-1], 1024)
+for p in chan_pGBs:
+    avg_pers = [ p / (p + 1/avg_bad) for avg_bad in brs ]
+    plt.plot(brs, avg_pers, label="pGB = {:.0e}".format(p))
+#plt.ylim(1e-8, 1)
+plt.xlabel('E[#_B]')
+plt.ylabel('Avg. channel PER')
+plt.legend()
+plt.grid()
+plt.savefig('markov_avg_per.pdf', format='pdf')
+
+plt.figure()
+plt.gca().set_yscale('log')
 
 for (j, p) in enumerate(fixed_params):
     mib_pers = [ r.avg_pers[0] for r in result_matrix[j] ]
@@ -78,17 +87,17 @@ for (j, p) in enumerate(fixed_params):
              marker='o',
              linewidth=1.5,
              label=("MIB K = {:d},"
-                    " e = {:.0e},"
+                    " p_GB = {:.0e},"
                     " t = {:.2f}".format(p['k'],
-                                         p['avg_per'],
+                                         p['chan_pGB'],
                                          p['overhead'])))
     plt.plot(avg_bad_runs, lib_pers,
              marker='o',
              linewidth=1.5,
              label=("LIB K = {:d},"
-                    " e = {:.0e},"
+                    " p_GB = {:.0e},"
                     " t = {:.2f}".format(p['k'],
-                                         p['avg_per'],
+                                         p['chan_pGB'],
                                          p['overhead'])))
 
 plt.ylim(1e-8, 1)
