e99c556d5a35e38c9d38884300d0882d60196e19
Riccardo Zanol <ricc@zanol.eu>
2017-11-16 15:59:58 +0100

    Merge simulation results

--- plot_uep_iid.py
+++ plot_uep_iid.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import sys
 
+import boto3
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,7 +11,6 @@ from uep import *
 from utils.aws import *
 from utils.plots import *
 from utils.stats import *
-
 
 class param_filters:
     @staticmethod
@@ -49,11 +49,13 @@ if __name__ == "__main__":
                                      allow_abbrev=False)
     parser.add_argument("--param_filter", help="How to filter the data",
                         type=str, default="all")
+    parser.add_argument("--merge", action='store_true',
+                        help="Merge the data packs")
     args = parser.parse_args()
 
-    data = load_data_prefix("uep_iid_final/")
+    data = load_data_prefix_2("uep_iid_final/")
 
-    git_sha1_set = sorted(set(d.get('git_sha1') or 'None' for d in data))
+    git_sha1_set = sorted(set(d[0].get('git_sha1') or 'None' for d in data))
     print("Found {:d} commits:".format(len(git_sha1_set)))
     for s in git_sha1_set:
         print("  - " + s)
@@ -62,41 +64,47 @@ if __name__ == "__main__":
     #     "57551110162e9e4f4d2094c1d86c6686a501fd96",
     # ]
 
-    # data = [d for d in data if d.get('git_sha1') in wanted_commits]
+    # data = [d for d in data if d[0].get('git_sha1') in wanted_commits]
 
     print("Using {:d} data packs".format(len(data)))
 
-    param_set = sorted(set((tuple(d['Ks']),
-                            tuple(d['RFs']),
-                            d['EF'],
-                            d['c'],
-                            d['delta'],
-                            d['iid_per']) for d in data))
+    if args.merge:
+        print("WARNING: The data packs will be merged by"
+              " averaging. The commit, timestamp and rng"
+              " seed will be lost.")
+        yn = input("Proceed? yes/[no]: ")
+        if yn.strip() not in ['yes', 'y']:
+            sys.exit(1)
 
-    # Old results no not have the avg_ripples
-    # for d in data:
-    #     if 'avg_ripples' not in d:
-    #         d['avg_ripples'] = [float('nan') for o in d['overheads']]
+    param_set = sorted(set((tuple(d[0]['Ks']),
+                            tuple(d[0]['RFs']),
+                            d[0]['EF'],
+                            d[0]['c'],
+                            d[0]['delta'],
+                            d[0]['iid_per']) for d in data))
 
     param_filter = getattr(param_filters, args.param_filter)
     assert(callable(param_filter))
 
     p = plots()
-    p.automaticXScale = True
-    #p.automaticXScale = [0,0.3]
+    p.automaticXScale = True #[0, 0.8]
     p.automaticYScale = [1e-8, 1]
     p.add_plot(plot_name='per',xlabel='Overhead',ylabel='PER',logy=True)
     p.add_plot(plot_name='nblocks',xlabel='Overhead',ylabel='nblocks',logy=False)
     #p.add_plot(plot_name='ripple',xlabel='Overhead',ylabel='ripple',logy=False)
     p.add_plot(plot_name='drop_rate',xlabel='Overhead',ylabel='drop_rate',logy=False)
 
+    merge_output = list()
     for params in param_set:
-        data_same_pars = [d for d in data if (tuple(d['Ks']),
-                                              tuple(d['RFs']),
-                                              d['EF'],
-                                              d['c'],
-                                              d['delta'],
-                                              d['iid_per']) == params]
+        data_same = [d for d in data if (tuple(d[0]['Ks']),
+                                         tuple(d[0]['RFs']),
+                                         d[0]['EF'],
+                                         d[0]['c'],
+                                         d[0]['delta'],
+                                         d[0]['iid_per']) == params]
+        data_same_pars = [d[0] for d in data_same]
+        data_same_keys = [d[1] for d in data_same]
+
         Ks = params[0]
         RFs = params[1]
         EF = params[2]
@@ -112,6 +120,7 @@ if __name__ == "__main__":
         nblocks = np.zeros(len(overheads), dtype=int)
         avg_ripples = np.zeros(len(overheads))
         avg_drop_rates = np.zeros(len(overheads))
+        error_counts = np.zeros((len(overheads), len(Ks)), dtype=int)
         for i, oh in enumerate(overheads):
             avg_counters = [AverageCounter() for k in Ks]
             avg_ripple = AverageCounter()
@@ -120,18 +129,44 @@ if __name__ == "__main__":
                 for l, d_oh in enumerate(d['overheads']):
                     if d_oh != oh: continue
 
+                    if hasattr(d['nblocks'], '__len__'):
+                        d_nblocks = d['nblocks'][l]
+                    else:
+                        d_nblocks = d['nblocks']
+
                     for j,k in enumerate(Ks):
-                        per = d['error_counts'][l][j] / (k * d['nblocks'])
-                        avg_counters[j].add(per, d['nblocks'])
+                        errc = d['error_counts'][l][j]
+                        error_counts[i, j] += errc
+                        per = errc / (k * d_nblocks)
+                        avg_counters[j].add(per, d_nblocks)
                     if not math.isnan(d['avg_ripples'][l]):
                         avg_ripple.add(d['avg_ripples'][l],
-                                       d['nblocks'])
-                    avg_drop.add(d['avg_drops'][l], d['nblocks'])
+                                       d_nblocks)
+                    avg_drop.add(d['avg_drops'][l], d_nblocks)
 
             avg_ripples[i] = avg_ripple.avg
             avg_drop_rates[i] = avg_drop.avg
             avg_pers[i,:] = [c.avg for c in avg_counters]
             nblocks[i] = avg_counters[0].total_weigth
+
+        if args.merge:
+            merged = {
+                'timestamp': datetime.datetime.now().timestamp(),
+                'merged': True,
+                'Ks': Ks,
+                'RFs': RFs,
+                'EF': EF,
+                'c': c,
+                'delta': delta,
+                'iid_per': iid_per,
+                'overheads': overheads,
+                'nblocks': nblocks,
+                'avg_pers': avg_pers,
+                'error_counts': error_counts,
+                'avg_drops': avg_drop_rates,
+                'avg_ripples': avg_ripples,
+            }
+            merge_output.append((merged, data_same_keys))
 
         if not param_filter(*params): continue
 
@@ -182,6 +217,21 @@ if __name__ == "__main__":
         #    print(" " * 4 + legend_str, end="")
         #    print(" -> MIB={:e}, LIB={:e}".format(avg_pers[the_oh_i, 0],
         #                                          avg_pers[the_oh_i, 1]))
+
+    for m in merge_output:
+        newid = random.getrandbits(64)
+        newkey = "uep_iid_final/uep_vs_oh_iid_{:d}_merged.pickle.xz".format(newid)
+        save_data(newkey, **(m[0]))
+        print("Uploaded merged data pack: " + newkey)
+        bucket = boto3.resource('s3').Bucket('uep.zanol.eu')
+        for k in m[1]:
+            bucket.copy({'Bucket': 'uep.zanol.eu', 'Key': k},
+                        "backup/"+k,
+                        ExtraArgs={'ACL': 'public-read'})
+        print("Backed up old packs")
+        bucket.delete_objects(Delete={'Objects':[{'Key':k} for k in m[1]]})
+        print("Deleted old packs")
+
 
     datestr = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     save_plot_pdf(p.get_plot('per'),'iid/{}/{} {}'.format(args.param_filter,
--- plot_uep_markov.py
+++ plot_uep_markov.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import sys
 
+import boto3
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -64,11 +65,13 @@ if __name__ == "__main__":
                                      allow_abbrev=False)
     parser.add_argument("--param_filter", help="How to filter the data",
                         type=str, default="all")
+    parser.add_argument("--merge", action='store_true',
+                        help="Merge the data packs")
     args = parser.parse_args()
 
-    data = load_data_prefix("uep_markov_final/")
+    data = load_data_prefix_2("uep_markov_final/")
 
-    git_sha1_set = sorted(set(d.get('git_sha1') or 'None' for d in data))
+    git_sha1_set = sorted(set(d[0].get('git_sha1') or 'None' for d in data))
     print("Found {:d} commits:".format(len(git_sha1_set)))
     for s in git_sha1_set:
         print("  - " + s)
@@ -78,18 +81,26 @@ if __name__ == "__main__":
     #     "a616ad6478ac1f80dc3f7983cbf5b4958bf9fcfb",
     # ]
 
-    # data = [d for d in data if d.get('git_sha1') in wanted_commits]
+    # data = [d for d in data if d[0].get('git_sha1') in wanted_commits]
 
     print("Using {:d} data packs".format(len(data)))
 
-    param_set = sorted(set((tuple(d['RFs']),
-                            d['EF'],
-                            d['c'],
-                            d['delta'],
-                            d['overhead'],
-                            d['avg_per'],
-                            d['avg_bad_run'],
-                            tuple(d['Ks_frac'])) for d in data))
+    if args.merge:
+        print("WARNING: The data packs will be merged by"
+              " averaging. The commit, timestamp and rng"
+              " seed will be lost.")
+        yn = input("Proceed? yes/[no]: ")
+        if yn.strip() not in ['yes', 'y']:
+            sys.exit(1)
+
+    param_set = sorted(set((tuple(d[0]['RFs']),
+                            d[0]['EF'],
+                            d[0]['c'],
+                            d[0]['delta'],
+                            d[0]['overhead'],
+                            d[0]['avg_per'],
+                            d[0]['avg_bad_run'],
+                            tuple(d[0]['Ks_frac'])) for d in data))
 
     param_filter = getattr(param_filters, args.param_filter)
     assert(callable(param_filter))
@@ -97,19 +108,24 @@ if __name__ == "__main__":
     p = plots()
     p.automaticXScale = True
     #p.automaticXScale = [0,0.3]
+    p.automaticYScale = [1e-8, 1]
     p.add_plot(plot_name='per',xlabel='K',ylabel='PER',logy=True)
     p.add_plot(plot_name='nblocks',xlabel='K',ylabel='nblocks',logy=False)
     p.add_plot(plot_name='drop_rate',xlabel='K',ylabel='drop_rate',logy=False)
 
+    merge_output = list()
     for params in param_set:
-        data_same_pars = [d for d in data if (tuple(d['RFs']),
-                                              d['EF'],
-                                              d['c'],
-                                              d['delta'],
-                                              d['overhead'],
-                                              d['avg_per'],
-                                              d['avg_bad_run'],
-                                              tuple(d['Ks_frac'])) == params]
+        data_same = [d for d in data if (tuple(d[0]['RFs']),
+                                         d[0]['EF'],
+                                         d[0]['c'],
+                                         d[0]['delta'],
+                                         d[0]['overhead'],
+                                         d[0]['avg_per'],
+                                         d[0]['avg_bad_run'],
+                                         tuple(d[0]['Ks_frac'])) == params]
+        data_same_pars = [d[0] for d in data_same]
+        data_same_keys = [d[1] for d in data_same]
+
         RFs = params[0]
         EF = params[1]
         c = params[2]
@@ -118,6 +134,8 @@ if __name__ == "__main__":
         avg_per = params[5]
         avg_bad_run = params[6]
         Ks_frac = params[7]
+        pGB = data_same_pars[0]['pGB']
+        pBG = data_same_pars[0]['pBG']
 
         k_blocks = sorted(set(k for d in data_same_pars for k in d['k_blocks']))
 
@@ -125,21 +143,51 @@ if __name__ == "__main__":
         nblocks = np.zeros(len(k_blocks), dtype=int)
         avg_drop_rates = np.zeros(len(k_blocks))
         used_Ks = np.zeros((len(k_blocks), len(Ks_frac)), dtype=int)
+        error_counts = np.zeros((len(k_blocks), len(Ks_frac)), dtype=int)
         for i, k_block in enumerate(k_blocks):
             avg_counters = [AverageCounter() for k in Ks_frac]
             avg_drop = AverageCounter()
             for d in data_same_pars:
                 for l, d_k_block in enumerate(d['k_blocks']):
                     if d_k_block != k_block: continue
+
+                    if hasattr(d['nblocks'], '__len__'):
+                        d_nblocks = d['nblocks'][l]
+                    else:
+                        d_nblocks = d['nblocks']
+
                     for j, _ in enumerate(Ks_frac):
-                        per = (d['error_counts'][l][j] /
-                               (d['used_Ks'][l][j] * d['nblocks']))
-                        avg_counters[j].add(per, d['nblocks'])
-                    avg_drop.add(d['avg_drops'][l], d['nblocks'])
+                        errc = d['error_counts'][l][j]
+                        error_counts[i, j] += errc
+                        per = errc / (d['used_Ks'][l][j] * d_nblocks)
+                        avg_counters[j].add(per, d_nblocks)
+                    avg_drop.add(d['avg_drops'][l], d_nblocks)
                     used_Ks[i] = d['used_Ks'][l]
             avg_pers[i,:] = [c.avg for c in avg_counters]
             nblocks[i] = avg_counters[0].total_weigth
             avg_drop_rates[i] = avg_drop.avg
+
+        if args.merge:
+            merged = {
+                'timestamp': datetime.datetime.now().timestamp(),
+                'merged': True,
+                'k_blocks': k_blocks,
+                'Ks_frac': Ks_frac,
+                'RFs': RFs,
+                'EF': EF,
+                'c': c,
+                'delta': delta,
+                'overhead': overhead,
+                'avg_per': avg_per,
+                'avg_bad_run': avg_bad_run,
+                'pGB': pGB,
+                'pBG': pBG,
+                'nblocks': nblocks,
+                'avg_pers': avg_pers,
+                'error_counts': error_counts,
+                'avg_drops': avg_drop_rates,
+            }
+            merge_output.append((merged, data_same_keys))
 
         if not param_filter(*params): continue
 
@@ -189,6 +237,21 @@ if __name__ == "__main__":
         #    print(" " * 4 + legend_str, end="")
         #    print(" -> MIB={:e}, LIB={:e}".format(avg_pers[the_oh_i, 0],
         #                                          avg_pers[the_oh_i, 1]))
+
+    for m in merge_output:
+        newid = random.getrandbits(64)
+        newkey = "uep_markov_final/uep_vs_k_markov_{:d}_merged.pickle.xz".format(newid)
+        save_data(newkey, **(m[0]))
+        print("Uploaded merged data pack: " + newkey)
+        bucket = boto3.resource('s3').Bucket('uep.zanol.eu')
+        for k in m[1]:
+            bucket.copy({'Bucket': 'uep.zanol.eu', 'Key': k},
+                        "backup/"+k,
+                        ExtraArgs={'ACL': 'public-read'})
+        print("Backed up old packs")
+        bucket.delete_objects(Delete={'Objects':[{'Key':k} for k in m[1]]})
+        print("Deleted old packs")
+
 
     datestr = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     save_plot_pdf(p.get_plot('per'),'markov/{}/{} {}'.format(args.param_filter,
